3eecbdb5bc66cf07394c652ff1e3e554a532eddc
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-03-22 21:52:39 +0100

    add DOM on score page and podium

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
 // THEME SELECTOR //
 // themeswitch() = {
 
 // }
-
 
 // QUIZ SELECTOR //
 // firstTheme.addEventListener("click", function() {
@@ -34,86 +32,83 @@
 
 // QUIZ FUNCTIONS //
 const firstQuizz = [
-    {
-        question : "In which year was Javascript Created ?",
-        answers : {
-            a : 1982,
-            b : 1918,
-            c : 1929,
-            d: 'the reponse d'
-        },
-        correctAnswer : "answer-a"
+  {
+    question: "In which year was Javascript Created ?",
+    answers: {
+      a: 1982,
+      b: 1918,
+      c: 1929,
+      d: "the reponse d",
     },
-    {
-        question : "Who invented Javascript?",
-        answers : {
-            a : "Magloire",
-            b : "DJ Snake",
-            c : "Brendan Eich",
-            d: 'the reponse d'
-        },
-        correctAnswer : "answer-c"
+    correctAnswer: "answer-a",
+  },
+  {
+    question: "Who invented Javascript?",
+    answers: {
+      a: "Magloire",
+      b: "DJ Snake",
+      c: "Brendan Eich",
+      d: "the reponse d",
     },
-    {
-        question : "Who invented Javascript?",
-        answers : {
-            a : "lalalal",
-            b : "DJ Snakakaajaje",
-            c : "Brendan Eich",
-            d: 'the reponse d'
-        },
-        correctAnswer : "answer-c"
-    }
-]
+    correctAnswer: "answer-c",
+  },
+  {
+    question: "Who invented Javascript?",
+    answers: {
+      a: "lalalal",
+      b: "DJ Snakakaajaje",
+      c: "Brendan Eich",
+      d: "the reponse d",
+    },
+    correctAnswer: "answer-c",
+  },
+];
 
-const secondQuizz = []
+const secondQuizz = [];
 
-const thirdQuizz = []
+const thirdQuizz = [];
 
-const createQuestion = (currentQuestion,questionNumber) => {
-    //create the card that will contain question title and answers
-    const quizz = document.querySelector(".quizz");
-    const questionCard = document.createElement("div");
-    questionCard.classList.add("card");
-    quizz.appendChild(questionCard);
+const createQuestion = (currentQuestion, questionNumber) => {
+  //create the card that will contain question title and answers
+  const quizz = document.querySelector(".quizz");
+  const questionCard = document.createElement("div");
+  questionCard.classList.add("card");
+  quizz.appendChild(questionCard);
 
-    //create the card title that will hold the title
-    const card=document.querySelector(".card")
-    const questionTitle = document.createElement("h1");
-    questionTitle.classList.add("questionTitle");
-    questionTitle.innerHTML=`Question #${questionNumber} - ${currentQuestion.question}`;
-    questionCard.appendChild(questionTitle);
+  //create the card title that will hold the title
+  const card = document.querySelector(".card");
+  const questionTitle = document.createElement("h1");
+  questionTitle.classList.add("questionTitle");
+  questionTitle.innerHTML = `Question #${questionNumber} - ${currentQuestion.question}`;
+  questionCard.appendChild(questionTitle);
 
-    //create the div that will hold the answers
-    const answerContainer=document.createElement("div");
-    answerContainer.classList.add("answer-container");
-    questionCard.appendChild(answerContainer);
+  //create the div that will hold the answers
+  const answerContainer = document.createElement("div");
+  answerContainer.classList.add("answer-container");
+  questionCard.appendChild(answerContainer);
 
-    //add a button for each answer
-    for(letter in currentQuestion.answers){
-        console.log(currentQuestion.answers[letter])
-        const answerLetter = document.createElement("label")
-        const oneAnswer = document.createElement("button")
-        oneAnswer.classList.add("cardButton");
-        oneAnswer.setAttribute('id',"answer-"+letter);
-        //oneAnswer.classList.add("color-2");
-        oneAnswer.innerHTML=`${letter}: ${currentQuestion.answers[letter]}`;
-        answerContainer.appendChild(oneAnswer);
-       
-    }
-}
+  //add a button for each answer
+  for (letter in currentQuestion.answers) {
+    console.log(currentQuestion.answers[letter]);
+    const answerLetter = document.createElement("label");
+    const oneAnswer = document.createElement("button");
+    oneAnswer.classList.add("cardButton");
+    oneAnswer.setAttribute("id", "answer-" + letter);
+    //oneAnswer.classList.add("color-2");
+    oneAnswer.innerHTML = `${letter}: ${currentQuestion.answers[letter]}`;
+    answerContainer.appendChild(oneAnswer);
+  }
+};
 
-// Etape 2 
+// Etape 2
 // Stocker le resultat de l'input dans un object
 
 //  const userInfo = {
-//     score: 0, 
+//     score: 0,
 //     name: ""
 // }
 
 // const selectTheme
-
-
 
 //une fois que le user clique sur démarrer dans la div d'avant
 //créer 3 tableaux par thème de question
@@ -123,8 +118,8 @@ const createQuestion = (currentQuestion,questionNumber) => {
 // inititalement quand on clique sur le bouton start
 //lancer quizzFunc(return)
 const quizzStart = (RETURN) => {
-    createQuestion(RETURN[0],0);
-}
+  createQuestion(RETURN[0], 0);
+};
 
 quizzStart(firstQuizz); // Test
 // let score = 0
@@ -143,9 +138,6 @@ quizzStart(firstQuizz); // Test
 //modifier les dom (vert, rouge, +score)
 //bouton next question qui apparait
 
-
-
-
 // let question = 0
 
 // func buttonNext = {
@@ -154,11 +146,69 @@ quizzStart(firstQuizz); // Test
 //     createQuestion
 // }
 
+//button .onclick
+const slideUp = (div) => {
+  document.querySelector(div).classList.replace("hid-box", "hid-box-hover");
+};
+
+
+// // PAGE RESULTAT
+
+const usertoto = {
+  userName: "Chouchou",
+  userScore: 7,
+  userRank: 666,
+};
+
+const userList = [
+  {userName: "Loris",
+  userScore: 7},
+
+  {userName: "Nicolas",
+  userScore: 2},
+
+  {userName: "Julien",
+  userScore: 10},
+
+  {userName: "Paul",
+  userScore: 6},
+  
+  {userName: "Zakari",
+  userScore: 1},
+];
+
+//pour afficher les score et rank
+
+const pCongrats = document.querySelector("#congrats");
+pCongrats.textContent = `Congrats ${usertoto.userName}, you scored is ${usertoto.userScore}/10.`;
+
+const pRank = document.querySelector("#rank");
+pRank.textContent = `You ranked #${usertoto.userRank}.`;
+
+//fonction pour trier le tableau userlist en fonction du résultat
+
+userList.sort(function (a, b) {
+  return b.userScore - a.userScore;
+});
+
+const firstUser = document.querySelector("#firstUserName");
+firstUser.textContent = `${userList[0].userName}`;
+
+const firstScore= document.querySelector("#firstUserScore");
+firstScore.textContent = `${userList[0].userScore}/10`;
+
+const secondUser = document.querySelector("#secondUserName");
+secondUser.textContent = `${userList[1].userName}`;
+
+const secondScore= document.querySelector("#secondUserScore");
+secondScore.textContent = `${userList[1].userScore}/10`;
+
+const thirdUser = document.querySelector("#thirdUserName");
+thirdUser.textContent = `${userList[2].userName}`;
+
+const thirdScore= document.querySelector("#thirdUserScore");
+thirdScore.textContent = `${userList[2].userScore}/10`;
 
 
 
-
-//button .onclick 
-const slideUp = (div)=>{
-    document.querySelector(div).classList.replace("hid-box","hid-box-hover")
-}
+// // FIN PAGE RESULTAT
